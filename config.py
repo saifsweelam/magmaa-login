30f1cc48a136c16002931a95f342c79d53ee3fad
Seif El-Din Sweilam <saifsweelam@gmail.com>
2020-12-11 00:56:46 +0200

    fix minor bugs

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') if os.environ.get('DATABASE_URL') else 'sqlite:///data.db'
 SECRET_KEY = 'super_secret'
